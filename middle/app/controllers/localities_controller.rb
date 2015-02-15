9d4d2e3c1cfe54ade5b85091e053fa68b9c34a5b
Adam Lomas <adamp@marcatodigital.com>
2015-02-15 09:13:15 -0500

    Adds RSpec/Capybara and a feature test for locality imports.

--- middle/app/controllers/localities_controller.rb
+++ middle/app/controllers/localities_controller.rb
@@ -4,7 +4,23 @@ class LocalitiesController < ApplicationController
 	end
 
 	def import
-		Locality.import(params[:file])
-		redirect_to root_url, notice: "Products Imported."
+		import_file 
+	rescue
+		redirect_with_error("Something went wrong.")
 	end
+
+	private
+
+		def redirect_with_notice(notice)
+			redirect_to root_url, notice: notice
+		end
+
+		def redirect_with_error(error)
+			redirect_to root_url, :flash => { :error => error }	
+		end
+
+		def import_file
+			Locality.import(params[:file])
+			redirect_with_notice("Products Imported.")
+		end
 end
---
+++ middle/spec/features/locality_import_spec.rb
@@ -0,0 +1,27 @@
+require "rails_helper"
+
+feature "Importing localities" do
+	
+	scenario "User imports a valid csv file with Sherbrooke, NS (pop 325), Toronto, ON (pop 2,700,000) and Victoria, BC (pop 200,000)" do
+		file_path = 'spec/fixtures/files/valid_locality_import_spec.csv'
+		visit localities_path
+		attach_file('file', file_path)
+		click_on "Import"
+		expect(page).to have_css("tr", :count => 4)
+		expect(page).to have_css("td", :text => "326")
+	end
+
+	scenario "User imports an invalid csv file" do
+		file_path = 'spec/fixtures/files/invalid_locality_import_spec.csv'
+		visit localities_path
+		attach_file('file', file_path)
+		click_on "Import"
+		expect(page).to have_css("div.alert-error")
+	end
+
+
+
+
+	
+
+end
